b2e4df1045b90e1eee01ad31a76cea49d093545f
Timothy Cope <kirbycope@gmail.com>
2025-01-15 15:10:23 -0800

    camera raycast position

--- scenes/minecraft/main.gd
+++ scenes/minecraft/main.gd
@@ -16,5 +16,8 @@ func _ready() -> void:
 	# Set camera's position
 	$Player/CameraMount/Camera3D.position = Vector3(0.0, 0.0, 0.0)
 
+	# Set the camera's raycast position to match the camera's position
+	$Player/CameraMount/Camera3D/RayCast3D.position = Vector3.ZERO
+
 	# Align visuals with the camera
 	$Player/Visuals.rotation = Vector3(0.0, 0.0, $Player/CameraMount.rotation.z)
